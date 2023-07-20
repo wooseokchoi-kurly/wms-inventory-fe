e005e6a6a8696e284b7e63f35dfdbd3a683f54ee
wooseokchoi-kurly <wooseok.choi@kurlycorp.com>
2023-07-20 20:40:50 +0900

    fea: menu toggle 추가

--- app/layout/InventoryAppBar.tsx
+++ app/layout/InventoryAppBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -15,27 +15,34 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import ListItemText from "@mui/material/ListItemText";
-import Divider from "@mui/material/Divider";
 import Collapse from "@mui/material/Collapse";
 import StarBorder from "@mui/icons-material/StarBorder";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import { useRouter } from "next/navigation";
 
-type toggleStateType = "listItem" | "";
+interface ListObject {
+  id: number;
+  code: string;
+  name: string;
+  isHidden: boolean;
+  listItem: ListItemObject[];
+}
 
 interface ListItemObject {
+  id: number;
+  code: string;
   name: string;
-  isOpen: boolean;
 }
 
 export default function InventoryAppBar() {
   const router = useRouter();
   const [drawerState, setDrawerState] = useState(false);
-  const [listItemOpenState, setListItemOpenState] = React.useState(true);
-  const [listItemOpenState2, setListItemOpenState2] = React.useState<
-    ListItemObject[]
-  >([]);
+  const [listItemOpenState, setListItemOpenState] = React.useState<
+    Map<string, boolean>
+  >(new Map<string, boolean>());
+  const [menuData, setMenuData] = useState<ListObject[]>();
+
   const toggleDrawer =
     (value: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
       if (
@@ -49,62 +56,63 @@ export default function InventoryAppBar() {
       setDrawerState(value);
     };
 
-  const toggleState = (type: toggleStateType) => {
-    if (type === "listItem") {
-      setListItemOpenState(!listItemOpenState);
-    } else {
-      setDrawerState(!drawerState);
-    }
+  const toggleState = (menuCode: string) => {
+    const map = new Map<string, boolean>(listItemOpenState.entries());
+    map.set(menuCode, !map.get(menuCode));
+
+    setListItemOpenState(map);
   };
 
-  const list = () => (
-    <Box
-      sx={{ width: 300 }}
-      role="presentation"
-      onKeyDown={toggleDrawer(false)}
-    >
-      <List>
-        <ListItemButton onClick={() => toggleState("listItem")}>
-          <ListItemIcon>
-            <InboxIcon />
-          </ListItemIcon>
-          <ListItemText primary="MSC" />
-          {listItemOpenState ? <ExpandLess /> : <ExpandMore />}
-        </ListItemButton>
-        <Collapse in={listItemOpenState} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            <ListItemButton
-              sx={{ pl: 4 }}
-              onClick={() => {
-                router.push("/pages/msc/decant");
-                setDrawerState(false);
-              }}
-            >
-              <ListItemIcon>
-                <StarBorder />
-              </ListItemIcon>
-              <ListItemText primary="decant" />
-            </ListItemButton>
-          </List>
-          <List component="div" disablePadding>
-            <ListItemButton
-              sx={{ pl: 4 }}
-              onClick={() => {
-                router.push("/pages/msc/gtp");
-                setDrawerState(false);
-              }}
-            >
-              <ListItemIcon>
-                <StarBorder />
-              </ListItemIcon>
-              <ListItemText primary="gtp" />
-            </ListItemButton>
-          </List>
-        </Collapse>
-      </List>
-      <Divider />
-    </Box>
-  );
+  useEffect(() => {
+    // todo: axios 를 이용해서 data 를 가져오는 방식으로 변경
+    const menuDataList = (): ListObject[] => {
+      return [
+        {
+          id: 1,
+          code: "msc",
+          name: "msc",
+          isHidden: false,
+          listItem: [
+            {
+              id: 1,
+              code: "decant",
+              name: "decant",
+            },
+            {
+              id: 2,
+              code: "gtp",
+              name: "gtp",
+            },
+          ],
+        },
+        {
+          id: 2,
+          code: "transaction",
+          name: "재고 트랜잭션",
+          isHidden: false,
+          listItem: [
+            {
+              id: 1,
+              code: "history",
+              name: "재고 이력",
+            },
+          ],
+        },
+      ];
+    };
+
+    setMenuData(menuDataList);
+  }, []);
+
+  useEffect(() => {
+    if (menuData != null) {
+      const menuItemMap = new Map<string, boolean>();
+
+      menuData.map((data) => {
+        setListItemOpenState(menuItemMap.set(data.code, false));
+      });
+    }
+  }, [menuData]);
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -116,18 +124,83 @@ export default function InventoryAppBar() {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
-            onClick={() => toggleState("")}
+            onClick={() => setDrawerState(!drawerState)}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1 }}
+            style={{ cursor: "pointer" }}
+            onClick={() => router.push("/")}
+          >
             IMS ADMIN
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
       <Drawer anchor={"left"} open={drawerState} onClose={toggleDrawer(false)}>
-        {list()}
+        <Box
+          sx={{ width: 300 }}
+          role="presentation"
+          onKeyDown={toggleDrawer(false)}
+        >
+          {menuData != null &&
+            menuData.map((data: ListObject) => {
+              return (
+                <>
+                  <List key={data.id}>
+                    <ListItemButton
+                      onClick={() => {
+                        toggleState(data.code);
+                        return listItemOpenState?.get(data.code);
+                      }}
+                    >
+                      <ListItemIcon>
+                        <InboxIcon />
+                      </ListItemIcon>
+                      <ListItemText primary={data.name} />
+                      {listItemOpenState?.get(data.code) ? (
+                        <ExpandLess />
+                      ) : (
+                        <ExpandMore />
+                      )}
+                    </ListItemButton>
+                    <Collapse
+                      in={listItemOpenState?.get(data.code)}
+                      timeout="auto"
+                      unmountOnExit
+                      key={data.id}
+                    >
+                      {data.listItem.map((item: ListItemObject) => {
+                        return (
+                          <>
+                            <List component="div" disablePadding key={item.id}>
+                              <ListItemButton
+                                sx={{ pl: 4 }}
+                                onClick={() => {
+                                  router.push(
+                                    `/pages/${data.code}/${item.code}`,
+                                  );
+                                  setDrawerState(false);
+                                }}
+                              >
+                                <ListItemIcon>
+                                  <StarBorder />
+                                </ListItemIcon>
+                                <ListItemText primary={item.name} />
+                              </ListItemButton>
+                            </List>
+                          </>
+                        );
+                      })}
+                    </Collapse>
+                  </List>
+                </>
+              );
+            })}
+        </Box>
       </Drawer>
     </Box>
   );
--- app/layout/InventoryFooter.tsx
+++ app/layout/InventoryFooter.tsx
@@ -3,15 +3,9 @@ import * as React from "react";
 export default function InventoryFooter() {
   return (
     <>
-      <div
-        style={{
-          textJustify: "auto",
-          justifyContent: "center",
-          textAlign: "center",
-        }}
-      >
-        재고 프로덕트 footer
-      </div>
+      <footer>
+        <small>Copyright &copy;wooseok.choi</small>
+      </footer>
     </>
   );
 }
